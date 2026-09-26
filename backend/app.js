381e66f1a638f7753e882f0c950534fd672fe6a9
Aneela Fatima <aneelafatima369@gmail.com>
2026-09-26 10:47:23 +0500

    deployment

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path")
 
 app.use(express.json());
 app.use(cookieParser());
@@ -13,13 +14,16 @@ app.use(
     credentials: true,
   }),
 );
-app.use("/", express.static("uploads"));
+app.use("/", express.static(path.join(___dirname,"./uploads"));
+app.use("/E-Shop", (req,res)=>{
+  res.send("Hello world!");
+});
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV != "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
